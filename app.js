2541a4bac9cc51f46c41505492cae5a0855a7422
Hema <hemavathi.gadi@gmail.com>
2023-06-20 10:32:10 +0000

    Added app.use(express.json())

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+app.use(express.json());
 
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 
-const dbPath = path.join(__dirname, "todoApplication.db");
+const dbPath = path.join(__dirname, "aodoApplication.db");
 
 let db = null;
 
